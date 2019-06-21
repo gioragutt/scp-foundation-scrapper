f87afa36aaffa49a4b38ddd0b377c1a4dfc89642
Giora Guttsait <giora111@gmail.com>
2019-06-21 20:31:00 +0300

    Fix running modules with redit dependency without proving REDIS_HOST environment
    Fixes #8

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const config = mapEnv({
   rabbitmqPass: 'rabbitmq',
   connectionAttempts: '5',
   connectionRetryDelayMs: '1000',
-  redisHost: 'redis',
+  redisHost: 'localhost',
 });
 
 const fixedConfig = {
